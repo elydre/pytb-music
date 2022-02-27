39b852f13251dd7b93f4ffb313cf5e2b88e2998a
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-02-27 18:33:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,45 @@ import tkinter as tk
 
 fenettre = tk.Tk()
 fenettre.title("pYtb music")
-fenettre.geometry("300x400")
+fenettre.geometry("300x390")
 fenettre.resizable(width=False, height=False)
 
 class Recherche:
     def __init__(self):
-        # barre de recherche
+        self.resultat_butons = []
         self.barre = tk.Entry(fenettre, width=30)
-        self.barre.pack(side=tk.TOP, fill=tk.X)
+        self.barre.place(x=0, y=0, width=300, height=30)
         self.barre.bind("<Return>", self.recherche)
+        
+
     def recherche(self, event):
+        self.destroy()
+
         query = self.barre.get()
-        self.barre.delete(0, tk.END)
-        self.barre.insert(0, "recherche en cours...")
-        self.barre.update()
+
+        self.info = tk.Label(fenettre, text="Recherche en cours...")
+        self.info.place(x=0, y=30, width=300, height=30)
+        self.info.update()
+
         self.resultat = recherche(query)
-        self.barre.delete(0, tk.END)
-        self.barre.insert(0, "recherche terminée")
-        self.barre.update()
+
+        self.info.destroy()
         self.afficher()
+
     def afficher(self):
-        self.liste = tk.Listbox(fenettre, width=30)
-        self.liste.pack(side=tk.TOP, fill=tk.X)
-        for video in self.resultat:
-            self.liste.insert(tk.END, video.title)
-        self.liste.bind("<Double-Button-1>", self.download)
+        
+        i = 30
+        for video in self.resultat[:11]:
+            self.resultat_butons.append(tk.Button(fenettre, text=video.title, command=lambda video=video: self.add(video)))
+            self.resultat_butons[-1].place(x=0, y=i, width=300, height=30)
+            i += 30
+
+    def destroy(self, full=False):
+        for buton in self.resultat_butons:
+            buton.destroy()
+        self.resultat_butons = []
+        if full:
+            self.barre.destroy()
 
 def LP_recherche():
     Recherche()
@@ -40,8 +54,8 @@ def LP_play(): ...
 
 def LP_liste(): ...
 
-tk.Button(fenettre, text="recherche", command=LP_recherche).place(x=0, y=370, width=100, height=30)
-tk.Button(fenettre, text="play-liste", command=LP_play).place(x=100, y=370, width=100, height=30)
-tk.Button(fenettre, text="lecture", command=LP_liste).place(x=200, y=370, width=100, height=30)
+tk.Button(fenettre, text="recherche", command=LP_recherche).place(x=0, y=360, width=100, height=30)
+tk.Button(fenettre, text="play-liste", command=LP_play).place(x=100, y=360, width=100, height=30)
+tk.Button(fenettre, text="lecture", command=LP_liste).place(x=200, y=360, width=100, height=30)
 
 fenettre.mainloop()
